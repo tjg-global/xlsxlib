a5ea31e96c077c22027897c4a82fe34e281e2f0a
Tim Golden <tim.golden@global.com>
2023-05-22 15:05:24 +0100

    Allow for an "epilog" statement Rework logging so it works inside the multiprocessing jobs

--- xlsxlib/runsql.py
+++ xlsxlib/runsql.py
@@ -1,4 +1,5 @@
 import os, sys
+import argparse
 import collections
 from concurrent import futures
 import csv
@@ -11,6 +12,10 @@ from . import connections
 
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
+stdout_handler = logging.StreamHandler()
+stdout_handler.setLevel(logging.DEBUG)
+logger.addHandler(stdout_handler)
+
 
 SNOWFLAKE_ACCOUNT = "global.eu-west-1"
 SNOWFLAKE_WAREHOUSE = "DWH_DEV_SMALL"
@@ -21,30 +26,57 @@ SnowflakeInfo = collections.namedtuple(
     ["user", "password", "account", "warehouse", "role", "database"]
 )
 
-#
-# Spoke to Hal:
-# - File format: two columns (tab/comma-separated) where the first column
-#   is a batch/grouping identifier; the rest is the SQL to run
-# - Probably no need for a Snowflake database; HM to check
-# -
-#
-
 def get_query_batches(filepath):
+    """Read batches of SQL commands from a file
+
+    If the file simply contains SQL with no batching column, treat them
+    all as one batch. If there is an initial column, treat is as a batch
+    and run the different batches concurrently.
+    """
+    logger.info("Read batches from %s", filepath)
     with open(filepath) as f:
         reader = csv.reader(f)
         headers = next(reader)
         if len(headers) == 1:
+            logger.warning("No batch column found")
             rows = [["all"] + row for row in reader]
         else:
             rows = [row[:2] for row in reader]
 
-    batches = {}
-    for batch, lines in itertools.groupby(rows, lambda x: x[0]):
-        batches[batch] = [line for (batch, line) in lines]
+    batches = collections.defaultdict(list)
+    for batch, line in rows:
+        if batch not in ("xx"):
+            batches[batch].append(line)
+    logger.info("Found %d batches", len(batches))
     return batches
 
-def run_concurrent_sql(args):
-    snowflake_info, batch_id, lines = args
+def run_lines(batch_id, db, lines):
+    q = db.cursor()
+    try:
+        print("DUMMY: %d lines of SQL" % len(lines))
+        #~ q.executemany(lines)
+    finally:
+        q.close()
+
+def run_epilog(batch_id, db, epilog):
+    q = db.cursor()
+    try:
+        q.execute(epilog)
+        rows = q.fetchall()
+        if rows:
+            logger.info("Results for batch %s", batch_id)
+            logger.info("-" * 100)
+            for row in rows:
+                logger.info(" | ".join("%s" % i for i in row))
+            logger.info("-" * 100)
+    finally:
+        q.close()
+
+def run_sql(args):
+    snowflake_info, batch_id, all_lines = args
+    if not all_lines:
+        return "Nothing to do for batch %s" % batch_id
+    lines, epilog = all_lines[:-1], all_lines[-1]
     logger.info("Running %d lines for batch %s", len(lines), batch_id)
     db = snowflake.connector.connect(
         user=snowflake_info.user,
@@ -53,15 +85,14 @@ def run_concurrent_sql(args):
         warehouse=snowflake_info.warehouse,
         database=snowflake_info.database
     )
-    logger.info("Connected to Snowflake %s with warehouse %s", snowflake_info.account, snowflake_info.warehouse)
-
-    q = db.cursor()
     try:
-        q.executemany(lines)
+        if lines:
+            run_lines(batch_id, db, epilog)
+        if epilog.strip():
+            run_epilog(batch_id, db, epilog)
+        return "%d lines run for batch %s" % (len(lines), batch_id)
     finally:
-        q.close()
-
-    return "%d lines run for batch %s" % (len(lines), batch_id)
+        db.close()
 
 def run_batches(snowflake_info, batches):
     #
@@ -71,15 +102,14 @@ def run_batches(snowflake_info, batches):
     concurrent_batches = [(snowflake_info, batch_id, lines) for (batch_id, lines) in batches.items()]
     with futures.ProcessPoolExecutor(max_workers=4) as executor:
         for result in executor.map(run_sql, concurrent_batches):
-            logger.debug(result)
+            logger.info(result)
 
-def main(args):
-    sql_filename = args.sql_filename
+def run(args):
     #
     # Attempt to read the query batches from the file first:
     # if that fails we don't need to bother opening the db connection
     #
-    batches = get_query_batches(filepath)
+    batches = get_query_batches(args.filename)
 
     snowflake_info = SnowflakeInfo(
         args.snowflake_user or os.environ['DBT_PROFILES_USER'],
@@ -92,7 +122,7 @@ def main(args):
 
     run_batches(snowflake_info, batches)
 
-if __name__ == '__main__':
+def command_line():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="A file containing SQL statements to run, optionally preceded by a batch id")
     parser.add_argument("--snowflake_user", help="Snowflake username")
@@ -102,5 +132,7 @@ if __name__ == '__main__':
     parser.add_argument("--snowflake_role", help="Snowflake role")
     parser.add_argument("--snowflake_database", help="Snowflake database")
     args = parser.parse_args()
+    run(args)
 
-    main(args)
+if __name__ == '__main__':
+    command_line()
