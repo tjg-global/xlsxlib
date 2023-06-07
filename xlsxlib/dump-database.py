e9194e041d6e5c806e89fa0c371e010a765c2f41
Tim Golden <tim.golden@global.com>
2023-06-07 15:52:49 +0100

    Add code to take a database definition generated in Snowflake by GET_DDL and break it out into individual folders & files

---
+++ xlsxlib/dump-database.py
@@ -0,0 +1,30 @@
+import os, sys
+import re
+
+def main(filepath):
+    db_name, _ = os.path.splitext(os.path.basename(filepath))
+    print("Database:", db_name)
+    print(os.getcwd())
+    if not os.path.exists(db_name):
+        os.mkdir(db_name)
+
+    with open("c:/temp/mn.sql") as f:
+        text = f.read()
+
+    r1 = re.compile(r"create or replace[^;]*;", flags=re.DOTALL)
+    for obj in r1.findall(text):
+        line1 = obj.splitlines()[0].strip("(;")
+        line1 = line1[len("create or replace "):]
+        type, name = line1.split()
+        print(type, "=>", name)
+
+        type_dirpath = os.path.join(db_name, type.lower())
+        if not os.path.exists(type_dirpath):
+            os.mkdir(type_dirpath)
+
+        with open(os.path.join(type_dirpath, "%s.sql" % (name)), "w") as f:
+            f.write(obj)
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
+
