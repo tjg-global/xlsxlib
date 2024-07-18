01117272c591c6d61afac48535e112dbdd4fff6e
Tim Golden <tim.golden@global.com>
2024-07-18 09:09:36 +0100

    Default to by-database

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='xlsxlib',
-    version='1.20.0',
+    version='1.20.1',
     description='SQL to Excel',
     author='Tim Golden',
     author_email='tim.golden@global.com',
