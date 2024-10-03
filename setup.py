d325eaefe28758d9a03b222dce507bfd206c9af5
Tim Golden <tim.golden@global.com>
2024-10-03 13:46:18 +0100

    Nudge version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='xlsxlib',
-    version='1.20.1',
+    version='1.20.2',
     description='SQL to Excel',
     author='Tim Golden',
     author_email='tim.golden@global.com',
